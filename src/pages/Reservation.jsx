ab8d98a8e84397fe9e429d8304c19a041af6c323
valfont <valfontaine@hotmail.fr>
2022-01-26 12:13:21 +0100

    page reservation ok 1/3

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import RecapVoyage from "../components/RecapVoyage";
+import FormVoyage from "../components/FormVoyage";
+
+import "../style/footer.css";
+import "../style/accueil.css";
+import "../style/reservation.css";
 
 export default function Reservation() {
   return (
     <div>
-      <h1>Page Réservation</h1>
+      <Navbar />
+      <div className="containerRes">
+        <RecapVoyage />
+        <FormVoyage />
+      </div>
+      <Footer />
     </div>
   );
 }
