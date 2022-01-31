fdfb83d1248c51999b67b1684d80932d38f8a0ce
valfont <valfontaine@hotmail.fr>
2022-01-31 14:28:21 +0100

    reservation3 ok

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,33 @@ import "../style/card.css";
 import personne from "../assets/svg/personne.svg";
 import bagage from "../assets/svg/bagage.svg";
 
-export default function Card({ title, classe, image, texte, texte2 }) {
+export default function Card({
+  title,
+  classe,
+  image,
+  texte,
+  texte2,
+  classImage,
+  icon1,
+  icon2,
+}) {
   return (
-    <div className="blockRond">
-      <div className="rond1"></div>
-      <div className="card">
-        <h4 className="h4Card">{title}</h4>
-        <p> {classe}</p>
-        <img src={image} />
-        <div className="blockP">
-          <img src={personne} />
-          <p className="pCard"> {texte}</p>
-        </div>
-        <div className="blockP">
-          <img src={bagage} />
-          <p className="pCard"> {texte2}</p>
-        </div>
-        <button className="buttonCardAjouter" type="submit">
-          Ajouter
-        </button>
+    <fieldset className="card">
+      <legend className="rond1"></legend>
+      <h4 className="h4Card">{title}</h4>
+      <p> {classe}</p>
+      <img src={image} className={classImage} />
+      <div className="blockP">
+        <img src={icon1} />
+        <p className="pCard"> {texte}</p>
       </div>
-    </div>
+      <div className="blockP">
+        <img src={icon2} />
+        <p className="pCard"> {texte2}</p>
+      </div>
+      <button className="buttonCardAjouter" type="submit">
+        Ajouter
+      </button>
+    </fieldset>
   );
 }
--- src/components/VehiculesCards.jsx
+++ src/components/VehiculesCards.jsx
@@ -3,7 +3,13 @@ import "../style/card.css";
 import etape from "../assets/svg/etape.svg";
 import berline from "../assets/svg/berline.svg";
 import limousine from "../assets/svg/limousine.svg";
+import van from "../assets/svg/van.svg";
 import Card from "../components/Card";
+import bagageImg from "../assets/svg/bagageImg.svg";
+import personne from "../assets/svg/personne.svg";
+import bagage from "../assets/svg/bagage.svg";
+import siègeEnfant from "../assets/svg/siègeEnfant.svg";
+import siègeBebe from "../assets/svg/siègeBebe.svg";
 
 export default function VehiculesCards() {
   return (
@@ -11,6 +17,9 @@ export default function VehiculesCards() {
       <div className="etape">
         <img src={etape} />
       </div>
+
+      {/* --------------------------------------- Card container Voiture ----------------------------- */}
+
       <h4 className="h4V">2. JE CHOISIS MES VEHICULES</h4>
       <div className="cardContainer">
         <Card
@@ -19,23 +28,44 @@ export default function VehiculesCards() {
           image={berline}
           texte="MAX 3 PERSONNES"
           texte2="2 BAGAGES"
+          icon1={personne}
+          icon2={bagage}
         />
         <Card
           title="Limousines"
           classe="Classe S (ou similaire)"
           image={limousine}
+          classImage="imageCardL"
           texte="MAX 2/3 PERSONNES"
           texte2="2 BAGAGES"
+          icon1={personne}
+          icon2={bagage}
         />
         <Card
-          title="Limousines"
-          classe="Classe S (ou similaire)"
-          image={limousine}
-          texte="MAX 2/3 PERSONNES"
-          texte2="2 BAGAGES"
+          title="Van"
+          classe="Classe V (ou similaire)"
+          image={van}
+          classImage="imageCardV"
+          texte="MAX 7 PERSONNES"
+          texte2="6 BAGAGES"
+          icon1={personne}
+          icon2={bagage}
         />
       </div>
+
+      {/* --------------------------------------- Card container Option ----------------------------- */}
+
       <h4 className="h4V">OPTION SUPPLEMENTAIRE</h4>
+      <div className="cardContainer">
+        <Card
+          title="Van Bagages"
+          image={bagageImg}
+          texte2="Jusqu'a 15 Bagages"
+          icon2={bagage}
+        />
+        <Card title="Siège Enfant" image={siègeEnfant} classImage="imgSiège" />
+        <Card title="Siège Bébé" image={siègeBebe} classImage="imageSiègeBB" />
+      </div>
       <div className="total">
         <p className="pTotal">Coût Total</p>
         <p className="pTotal">350 €</p>
